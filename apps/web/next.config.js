8415e395a8276b8be66de19d1e6a081a0915d013
Nishith <nishithdharmaraj@gmail.com>
2026-07-30 16:15:35 +0530

    chore: revert static export, using next-on-pages instead

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-  output: 'export',
-  typescript: { ignoreBuildErrors: true }, 
-  eslint: { ignoreDuringBuilds: true }, 
-  productionBrowserSourceMaps: false 
-}; 
-module.exports = nextConfig;
+const nextConfig = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true }, productionBrowserSourceMaps: false }; module.exports = nextConfig;
