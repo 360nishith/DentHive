b974fddc5e94eff0a8a55f1226fc485cf03bfd77
Nishith <nishithdharmaraj@gmail.com>
2026-09-03 21:44:37 +0530

    chore: wipe all scratch scripts

--- check_subs_now.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const subs = await prisma.subscription.findMany();
-  console.log(JSON.stringify(subs, null, 2));
-}
-run().catch(console.error).finally(() => prisma.$disconnect());
--- test_templates.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('./node_modules/@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const t = await prisma.template.findMany();
-  console.log(JSON.stringify(t.map(x => x.name), null, 2));
-}
-run().finally(() => prisma.$disconnect());
